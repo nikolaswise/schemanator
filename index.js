6646aaa4fd37995024fd94edd5a1d92b69fb931b
Nikolas Wise <nikolaswise@gmail.com>
2021-10-18 15:27:46 -0700

    update the docs

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const estimatedCost = {
   currency: 'USD',
   value: '0'
 }
-const prepTime = 'PT5M'
-const performTime = 'PT25M'
+const prepTime = null
+const performTime = null
 const totalTime = 'PT30M'
-// @TODO these will probably want to get expanded into objects
-// that have name and image keys, since I think we'll want to 
-// use all the flexibillity available in the schema.
+
+// These arrays are both object with name and image keys
+// names are string, images are URLs
 const tools = [{
   name: 'Pencil',
   image: null
@@ -43,6 +43,9 @@ const pencil_steps = [
     },{
       text: "Draw another circle",
       type: "HowToDirection"
+    },{
+      text: "Sort of above the first circle.",
+      type: "HowToDirection"
     }
   ],[
      {
@@ -50,6 +53,9 @@ const pencil_steps = [
        type: "HowToDirection"
      },{
        text: "Don't fuck it up!",
+       type: "HowToTip"
+     },{
+       text: "It's not that hard",
        type: "HowToTip"
      } 
   ]
@@ -76,10 +82,12 @@ const ink_steps = [
 // Sections are an array of objets:
 // name: string denoting the _name_ of the section
 // steps: array, a steps array as described above.
-const how_to_draw_an_owl = [
+const how_to_cartoon_an_owl = [
   { name: 'Penciling', steps: pencil_steps },
   { name: 'Inking', steps: ink_steps }
 ]  
+
+const how_to_draw_an_owl = pencil_steps
 
 // Accepts a step array, as desribed above
 // Return a HowToStep object
@@ -108,21 +116,24 @@ const mapSections = (arr) => arr.map((section, i) => {
   }
 })
 
-// Accepts an array of strings
-// Returns a HowToTools array
-const mapTools = (arr) => arr.map(tool => {
-  return {
-    "@type": "HowToTool",
-    "name": tool
-  }
-})
+// Accept the data for the steps and reutrn either
+// the array of sections
+// or the array of steps
+const renderSteps = (arr) => {
+  return arr[0].name // @TODO lol this is probably kinda gross
+    ? mapSections(arr)
+    : mapSteps(arr)
+}
 
-// Accepts an array of strings
-// Returns a HowToSupplies array
-const mapSupplies = (arr) => arr.map(supply => {
+// Accepts a string either `HowToSupply` or `HowToTool`
+// Returns a fn:
+//   Accepts an array of strings
+//   Returns a HowToTools array
+const mapDependencies = (type) => (arr) => arr.map(({name, image}) => {
   return {
-    "@type": "HowToSupply",
-    "name": supply
+    "@type": type,
+    name,
+    image
   }
 })
 
@@ -133,20 +144,14 @@ const generateSchema = (data) => {
     "@type": "HowTo",
     name,
     description,
-    // prepTime,
-    // performTime,
-    // totalTime,
-    // tool: mapTools(tools),
-    // supply: mapSupplies(supplies),
-    // @TODO: This function needs to determing if it contains sections
-    // or not and either mapSections or mapSteps accordingly. 
-    // Should be able to check for existence of the `name` key on the
-    // first element.
-    "step": mapSections(data)
+    prepTime,
+    performTime,
+    totalTime,
+    tool: mapDependencies('HowToTool')(tools),
+    supply: mapDependencies('HowToSupply')(supplies),
+    "step": renderSteps(data)
   }
 }
-
-// generateSchema(how_to_draw_an_owl)
 
 console.log(`
 <script type="application/ld+json">
